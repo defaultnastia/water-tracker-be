5f6934436a3374f8349c49da6eaa8950681b32c3
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-09-14 22:47:33 +0300

    add some structure

---
+++ routes/waterRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const waterRouter = express.Router();
+
+export default waterRouter;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import waterRouter from "./routes/waterRouter.js";
+
 dotenv.config();
 
 const startServer = () => {
   const app = express();
   const port = Number(process.env.PORT);
+
+  app.use("/api/water", waterRouter);
 
   return app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
