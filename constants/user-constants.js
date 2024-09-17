24c010d52680429f5a9b9e76e161197f4169c675
nestertsovv <n.nestertsovv@gmail.com>
2024-09-17 13:09:52 +0300

    fix&feat: user modal fixes; update user middleware added

--- constants/user-constants.js
+++ constants/user-constants.js
@@ -2,3 +2,5 @@ export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 // at least 1 letter and 1 number (from 6 to 12 characters)
 export const passwordRegexp = /^(?=.*[0-9])(?=.*[a-z])(?!.* ).{6,12}$/;
+
+export const gendersList = ["male", "female"];
